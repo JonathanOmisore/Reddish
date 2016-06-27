eefc791f591360c6b10ab6ba5d114fd98cd625d0
comprogger <xiicubed@gmail.com>
2016-06-27 00:30:15 -0400

    add

---
+++ reddish.py
@@ -0,0 +1,29 @@
+import praw
+def seperate():
+    seperator = '=' * 60
+    return seperator
+    
+file = open('subreddits.txt', 'r')
+items = file.read().splitlines()
+for item in items:
+    if item == '':
+        items.remove(item)
+file.close()
+r = praw.Reddit("oh hi")
+
+
+for item in items:
+     
+    print(item + ":\n")
+    for x in range(0,len(items)):
+        
+        subreddit = r.get_subreddit(item)
+        try:
+            for submission in subreddit.get_hot(limit=10):
+                print(submission.title + "\n" + submission.url + "\n" + submission.permalink + "\n" + submission.selftext + "\n" + seperate() )
+        except praw.errors.InvalidSubreddit:
+             print(str(subreddit) + " is private or does not exist")
+            
+        
+    
+    
