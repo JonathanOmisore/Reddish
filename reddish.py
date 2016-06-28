f06de680b7c82cb6e01ed7082f767fb17ce10735
comprogger <xiicubed@gmail.com>
2016-06-28 19:34:26 -0400

    lowered number of posts and fixed glitch that repeated posts twice

--- reddish.py
+++ reddish.py
@@ -13,17 +13,21 @@ r = praw.Reddit("User Agent 007")
 
 
 for item in items:
-     
+    
     print(item + ":\n")
-    for x in range(0,len(items)):
+    subreddit = r.get_subreddit(item)
+    enter = input("")
+    
         
-        subreddit = r.get_subreddit(item)
-        try:
-            for submission in subreddit.get_hot(limit=10):
-                print(submission.title + "\n" + submission.url + "\n" + submission.permalink + "\n" + submission.selftext + "\n" + seperate() )
-                enter = input("Press any key to continue")
-        except praw.errors.InvalidSubreddit:
-             print(str(subreddit) + " is private or does not exist")
+        
+    try:
+            
+        for submission in subreddit.get_hot(limit=5):
+                
+            print(submission.title + "\n" + submission.url + "\n" + submission.permalink + "\n" + submission.selftext + "\n" + seperate() )
+                
+    except praw.errors.InvalidSubreddit:
+            print(str(subreddit) + " is private or does not exist")
             
         
     
