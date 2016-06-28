f023604dfcb952b97523f2b99005ca76e2538977
comprogger <godzillamario@gmail.com>
2016-06-28 19:04:28 -0400

    Update reddish.py

--- reddish.py
+++ reddish.py
@@ -18,26 +18,13 @@ for item in items:
     subreddit = r.get_subreddit(item)
     enter = input("")
     
-        
-<<<<<<< HEAD
-        
-    try:
-            
-        for submission in subreddit.get_hot(limit=5):
-                
-            print(submission.title + "\n" + submission.url + "\n" + submission.permalink + "\n" + submission.selftext + "\n" + seperate() )
-                
-    except praw.errors.InvalidSubreddit:
-            print(str(subreddit) + " is private or does not exist")
-=======
-        subreddit = r.get_subreddit(item)
+
         try:
-            for submission in subreddit.get_hot(limit=10):
+            for submission in subreddit.get_hot(limit=5):
                 print(submission.title + "\n" + submission.url + "\n" + submission.permalink + "\n" + submission.selftext + "\n" + seperate() )
-                enter = input("")
+                
         except praw.errors.InvalidSubreddit:
              print(str(subreddit) + " is private or does not exist")
->>>>>>> a0f5ab5417426eaa4e038b150d7667bc9152485a
             
         
     
